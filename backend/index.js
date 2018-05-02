54fbe97e62a1c04049a357289984640927f20084
Latence132 <alexandre.chaumet@gadz.org>
2018-05-02 18:14:12 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql')
+const express = require('express')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const cors = require('cors');
+const config = require('./config')
+const auth = require('./auth')
+
+
+try {
+  const db = mysql.createConnection(config.db)
+  const app = express()
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+
+  app.get('/test', (req, res) => {
+    db.query('select 1+1', (error, results) => {
+      if (error) return res.status(500).json({type: 'error', error})
+      res.json({type: 'success', message: 'Test OK', results})
+    })
+  })
+  app.use(cors())
+  app.use('/auth', auth({db, express, bcrypt, jwt, jwtToken: config.jwtToken}))
+
+  app.listen(config.port)
+  console.log('App is running on port ' + config.port)
+} catch (e) {
+  process.exit(1)
+}
---
+++ frontend/pages/login.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-layout justify-center align-center>
+    <v-card class="elevation-10" style="flex: 0 1 400px">
+      <v-card-title class="headline">Log In</v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="submit">
+          <v-alert v-if="alert" :type="alert.type" value="true">{{alert.message}}</v-alert>
+          <v-text-field label="Email" v-model="email"/>
+          <v-text-field label="Password" v-model="password" type="password"/>
+          <v-btn type="submit" :loading="loading" :disabled="loading">Log In</v-btn>
+          <v-btn color="red white--text" v-if="google_ready" @click="google_submit" :loading="google_loading" :disabled="google_loading">Log in with Google</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-layout>
+</template>
+<script>
+import {googleClientId} from '~/config'
+export default {
+  layout: 'fullscreen',
+  data () {
+    return {
+      email: '',
+      password: '',
+      alert: null,
+      loading: false,
+      google_loading: false,
+      google_ready: false
+    }
+  },
+  methods: {
+    submit () {
+      this.alert = null
+      this.loading = true
+      this.$store.dispatch('auth/login', {
+        email: this.email,
+        password: this.password
+      }).then(result => {
+        this.alert = {type: 'success', message: result.data.message}
+        this.loading = false
+        this.$router.push('/admin')
+      }).catch(error => {
+        console.log('error', error.response)
+        this.loading = false
+        if (error.response && error.response.data) {
+          this.alert = {type: 'error', message: error.response.data.message || error.reponse.status}
+        }
+      })
+    },
+    google_submit () {
+      if (!this.google_ready) return
+      this.alert = null
+      this.google_loading = true
+      window.google_auth2.signIn()
+        .then(() => this.$store.dispatch('auth/login_google', window.google_auth2.currentUser.get().Zi.access_token))
+        .then(result => {
+          this.alert = {type: 'success', message: result.data.message}
+          this.google_loading = false
+          this.$router.push('/admin')
+        })
+        .catch(error => {
+          this.google_loading = false
+          if (error.response && error.response.data) {
+            this.alert = {type: 'error', message: error.response.data.message || error.reponse.status}
+          }
+        })
+    }
+  },
+  mounted () {
+    window.gapiOnLoadCallback = () => {
+      window.gapi.load('auth2', () => {
+        window.google_auth2 = window.gapi.auth2.init({
+          client_id: googleClientId,
+          fetch_basic_profile: false,
+          scope: 'profile email'
+        })
+      })
+      this.google_ready = true
+    }
+    const installGoogleSdkScript = (d, s, id) => {
+      if (d.getElementById(id)) {
+        this.google_sdk_initialized = true
+        return
+      }
+      let fjs = d.getElementsByTagName(s)[0]
+      let js = d.createElement(s)
+      js.id = id
+      js.src = 'https://apis.google.com/js/platform.js?onload=gapiOnLoadCallback'
+      fjs.parentNode.insertBefore(js, fjs)
+    }
+    installGoogleSdkScript(document, 'script', 'google-jssdk')
+  }
+}
+</script>
